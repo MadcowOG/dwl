d20aa3b667af6ed073e88f1c1eb617bec1d4c300
MadcowOG <N/A>
2023-02-26 13:18:09 -0800

    focused_client should be 0 then 1 not -1 then not -1.

--- dwl.c
+++ dwl.c
@@ -1379,8 +1379,7 @@ dwl_output_printstatus_to(Monitor* monitor, DwlOutput *output)
 	zdwl_output_v1_send_active(output->resource, monitor == selmon);
 
 	for ( tag = 0 ; tag < LENGTH(tags); tag++) {
-		numclients = state = 0;
-		focused_client = -1;
+		numclients = state = focused_client = 0;
 		tagmask = 1 << tag;
 		if ((tagmask & monitor->tagset[monitor->seltags]) != 0)
 			state |= ZDWL_OUTPUT_V1_TAG_STATE_ACTIVE;
@@ -1391,7 +1390,7 @@ dwl_output_printstatus_to(Monitor* monitor, DwlOutput *output)
 			if (!(c->tags & tagmask))
 				continue;
 			if (c == focused)
-				focused_client = numclients;
+				focused_client = 1;
 			if (c->isurgent)
 				state |= ZDWL_OUTPUT_V1_TAG_STATE_URGENT;
 
